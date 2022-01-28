ae752e8389c817cc70959563d33f5819c6e3651a
Oliver Grimm <oliver.grimm@geowerkstatt.ch>
2022-01-28 13:37:10 +0100

    complete topics in model, adapt new model name

--- db/ili2pg dataimport.cmd
+++ db/ili2pg dataimport.cmd
@@ -8,4 +8,4 @@ REM 4: DB-User Password
 REM 5: Database-Name (zB. gewisso2)
 REM 6: Schema-Name (zB. gewisso)
 
-java.exe -jar .\ili2pg-4.6.0.jar --import --dbhost %1 --dbport %2 --dbusr %3 --dbpwd %4 --dbdatabase %5 --dbschema %6 --setupPgExt --coalesceCatalogueRef --createEnumTabs --createNumChecks --createUnique --createFk --createFkIdx --beautifyEnumDispName --createGeomIdx --createTypeConstraint --createEnumTabsWithId --createTidCol --smart2Inheritance --defaultSrsCode 2056 --disableValidation --skipReferenceErrors --models GewISSO_V2 ./GEWISSO2.xtf
+java.exe -jar .\ili2pg-4.6.0.jar --import --dbhost %1 --dbport %2 --dbusr %3 --dbpwd %4 --dbdatabase %5 --dbschema %6 --setupPgExt --coalesceCatalogueRef --createEnumTabs --createNumChecks --createUnique --createFk --createFkIdx --beautifyEnumDispName --createGeomIdx --createTypeConstraint --createEnumTabsWithId --createTidCol --smart2Inheritance --defaultSrsCode 2056 --disableValidation --skipReferenceErrors --models SO_AFU_Fliessgewaesser_20220228 ./GEWISSO2.xtf
